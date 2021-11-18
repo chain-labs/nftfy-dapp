1d8703987eb8698e1005423a1d2dfb0cc9f3e82b
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-11-19 03:18:47 +0530

    code shifted

--- src/components/OuterContainer.tsx
+++ src/components/OuterContainer.tsx
@@ -2,14 +2,14 @@ import Box from 'components/Box';
 // import Navbar from 'components/Navbar';
 // import Footer from './Footer';
 
-// const OuterContainer = ({ children, bg }) => {
-// 	return (
-// 		<Box bg={bg} minHeight="100vh">
-// 			<Navbar />
-// 			{children}
-// 			<Footer />
-// 		</Box>
-// 	);
-// };
+const OuterContainer = ({ children, bg }) => {
+	return (
+		<Box bg={bg} minHeight="100vh">
+			{/* <Navbar /> */}
+			{children}
+			{/* <Footer /> */}
+		</Box>
+	);
+};
 
-// export default OuterContainer;
+export default OuterContainer;
