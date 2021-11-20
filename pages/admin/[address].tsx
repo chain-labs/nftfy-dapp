7a448aad0f5457a1da1baedd288a6de36d6dd074
angel112 <ngllakra@gmail.com>
2021-11-20 14:41:41 +0530

    Created admin page with dynamic routes

---
+++ pages/admin/[address].tsx
@@ -0,0 +1,24 @@
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
+import Box from "src/components/Box";
+import { StatesContext } from "src/components/StatesContext";
+import AdminComp from "src/containers/admin";
+import useContract, { getContractDetails } from "src/ethereum/useContracts";
+
+const AdminPage = () => {
+  const state = useContext(StatesContext);
+  const router = useRouter();
+
+  const [contractAddress, setContractAddress] = useState<string>("");
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    setContractAddress(router.query.address as string);
+  }, [router.isReady]);
+
+  if (!state.provider || !state.signer) return null;
+  return <AdminComp contractAddress={contractAddress} />;
+};
+
+export default AdminPage;
