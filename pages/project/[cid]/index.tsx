3088f3eb84242421b55cddde1ef8f7ba1933ed3c
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-11-23 12:21:11 +0530

    Dynamic Routing Done

--- pages/project/[cid]/index.tsx
+++ pages/project/[cid]/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Head from "next/head";
 import ProjPageComp from "src/containers/projectpage";
+import { useRouter } from "next/router";
 
 const ProjectPage = () => {
+  const router = useRouter();
+  const { cid } = router.query;
   return (
     <React.Fragment>
       {/* <Head>
 				<title>Warrior</title>
 			</Head> */}
-      <ProjPageComp />
+      {cid ? <ProjPageComp cid={cid?.toString()} /> : ""}
     </React.Fragment>
   );
 };
--- src/containers/projectpage/logic.js
+++
@@ -1,3 +0,0 @@
-// presale Timer - isPresaleAllowed() && currentTime< presaleStartTime
-// const isPresaleAllowed //Method in collection Smart Contracts
-// presale buy - isPresaleAllowed() && isPresaleActive() //isPresaleActive Method in SC if both are true presale buy will come into picture
