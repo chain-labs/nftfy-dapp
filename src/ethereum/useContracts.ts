da1e887ec25d612c7b30039cc12a271894217f7f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-11-23 17:57:28 +0100

    maybe a fix

--- src/ethereum/useContracts.ts
+++ src/ethereum/useContracts.ts
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 import contracts from "../contracts/contracts.json";
 import { ProviderProps } from "./types";
 
-export const getContractDetails = (contractName, provider) => {
+export const getContractDetails = async (contractName, provider) => {
   if (provider?.provider) {
     console.log(provider);
-    const network = contracts["4"];
-    // const network = contracts[provider.getNetwork().chainId.toString()];
-    console.log(network, provider.getNetwork());
+    // const network = contracts["4"];
+    const network = contracts[(await provider.getNetwork()).chainId.toString()];
+    console.log(network,(await provider.getNetwork()).chainId.toString(), provider.getNetwork());
     const contractDetails =
       network[Object.keys(network)[0]].contracts[contractName];
     console.log(contractDetails)
@@ -29,10 +29,12 @@ const useContract = (contractName: string, provider: ProviderProps): any => {
     console.log("UseContract started",contractName, provider);
     if (providers.Provider.isProvider(provider)) {
       try {
-        const { address, abi } = getContractDetails(contractName, provider);
-        console.log("This is working");
-        console.log(address,abi, provider);
-        setContract(new ethers.Contract(address, abi, provider));
+        getContractDetails(contractName, provider)
+        .then(({address, abi}) => {
+          console.log("This is working");
+          console.log(address,abi, provider);
+          setContract(new ethers.Contract(address, abi, provider));
+        });
       } catch (error) {
         setContract(undefined);
         console.log(provider, contract);
